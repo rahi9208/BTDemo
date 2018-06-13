c3e69548dd3b033799fffc18de0cb0faafde5df3
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-13 15:53:08 +0530

    adding list endpoint

---
+++ BTDemo/list.js
@@ -0,0 +1,25 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+	let response = {
+		"isBase64Encoded": 1,
+		"statusCode": 200,
+		"headers": {
+			"headerName": "headerValue"
+		},
+		"body": "..."
+	};
+	let itemType = event.queryStringParameters.type;
+
+	ddb.scan({
+		TableName: 'BTMenu', ExpressionAttributeValues: { ':it': itemType }, FilterExpression: 'itemType = :it'
+	}, function (err, data) {
+		if(data.Items){
+			response.body = JSON.stringify(data.Items);
+		}
+		callback(err, response);
+	});
+
+
+}
